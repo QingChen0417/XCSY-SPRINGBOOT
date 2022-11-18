b05d0814b4f2ccc7f48e48af2d0994f664645f37
chenfuxin <1479935230@qq.com>
2022-11-18 17:37:56 +0800

    Changes

--- src/main/java/com/zzxy/xc/controller/PageController.java
+++ src/main/java/com/zzxy/xc/controller/PageController.java
@@ -1,6 +1,7 @@
 package com.zzxy.xc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,5 +11,17 @@ public class PageController {
 	public String doIndexUI() {
 		return "starter";
 	}
-	
+
+	//"{module}/{page}" : 可以统一接受访问路径
+	//参数需加@PathVariable()注解
+	@RequestMapping("{module}/{page}")
+	public String doModuleUI(@PathVariable("page") String page) {
+		return "sys/"+page;
+	}
+
+	@RequestMapping("doPageUI")
+	public String doPageUI() {
+		return "common/page";
+	}
+
 }
