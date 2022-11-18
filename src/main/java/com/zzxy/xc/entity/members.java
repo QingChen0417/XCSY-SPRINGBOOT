36331d86ba7cfeae9ee66cc15d42e67b6bf7906d
heyuwei <2199961294@qq.com>
2022-11-18 15:00:13 +0800

    1111

---
+++ src/main/java/com/zzxy/xc/entity/members.java
@@ -0,0 +1,18 @@
+package com.zzxy.xc.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import lombok.Data;
+
+//会员信息实体类
+@Data
+public class members implements Serializable{
+	private static final long serialVersionUID = 6087253410364854719L;
+	private Integer id;//商品ID
+	private String memberName;//成员名
+	private Date createdTime;//创建时间
+	private String createdUser;//创建的用户
+	private Date modifiedTime;//变质时间
+	private String modifiedUser;//修改的用户
+}
